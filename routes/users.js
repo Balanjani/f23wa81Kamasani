55e65a8d8d320c727b6ebce7d21976a6335cc01c
Balanjani kamasani <40666589+Balanjani@users.noreply.github.com>
2023-10-26 00:27:22 -0500

    Users Update

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
+ 
 var router = express.Router();
-
+ 
+ 
+ 
+let sum = 0;
+ 
+let more = 0;
+ 
 /* GET users listing. */
+ 
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+    more += 1;
+    sum += more;
+    res.send(`Sum is: ${sum}`);
+  });
